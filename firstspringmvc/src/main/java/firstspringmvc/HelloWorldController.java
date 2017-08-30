e75d8142ab09577ed36a73b7987af6869f8c5329
Arun Kumar Ram <arunram@policybazaar.com>
2017-08-30 17:11:27 +0530

    springmvc example

---
+++ firstspringmvc/src/main/java/firstspringmvc/HelloWorldController.java
@@ -0,0 +1,36 @@
+package firstspringmvc;
+
+import org.omg.CORBA.Request;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class HelloWorldController {
+	String message = "Welcome to Spring MVC!";
+ 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/abc/hello", method = RequestMethod.GET )
+	public ModelAndView displayMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
---
+++ firstspringmvc/src/main/java/firstspringmvc/firstRestController.java
@@ -0,0 +1,18 @@
+package firstspringmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class firstRestController{
+	
+	
+	@RequestMapping("/api/hello")
+	public @ResponseBody String hello()
+	{
+		return "hi........";
+	}
+	
+}
